fcbf73d4806a81dee03420a534e3160daa24ce47
CL <carolinemv.le@gmail.com>
2020-02-12 10:35:44 -0800

    Updates JS

--- js/main.js
+++ js/main.js
@@ -2,23 +2,29 @@
 
 const slotValues = [
 	{ name: 'dollar', image: 'images/dollar.png' },
-	{ name: 'heel', image: 'images/heel.png' }, 
+	{ name: 'pleaser', image: 'images/pleaser.png' }, 
 	{ name: 'cherries', image: 'images/cherries.png' },
 	{ name: 'kit', image: 'images/kit.png' }, 
 	{ name: 'angel', image: 'images/angel.png' },
-	{ name: 'devil', image: 'images/devil.png' }
+	{ name: 'devil', image: 'images/devil.png' },
+	{ name: 'of', image: 'images/of.png' },
+	{ name: 'mv', image: 'images/mv.png' }
   ];
 /* * * * * * * * * * APP'S STATE VARIABLES * * * * * * * * * */
+
 
 let cash, tkns;
 let spinResults =[];
 
+
 /* * * * * * * * * CASHED ELEMENT REFERENCES * * * * * * * * */
+
 
 let placeBet = document.querySelector('#pb'); // button
 let cashIn = document.querySelector('#cashin'); // input
 let cashOut = document.querySelector('#cashout'); // button
 let spinner = document.querySelector('#spinner');
+
 
 /* * * * * * * * * * * EVENT LISTENERS * * * * * * * * * * * */
 
@@ -29,7 +35,10 @@ spinner.addEventListener('click', function(evt){
         spin(evt);
     } 
     else {
-		cashIn.value = 'INSUFFICIENT FUNDS';
+		placeBet.style.background = 'red';
+		cashIn.style.color = 'red';
+		cashIn.style.borderColor = 'black';
+		cashIn.value = "IF YOU'RE STROKIN', TIP A TOKEN";
 	}
 });
 
@@ -38,83 +47,158 @@ spinner.addEventListener('click', function(evt){
 initialize();
 
 function initialize() {
-	console.log('GAME STARTED')
+	
+	// console.log('GAME STARTED')
+	
 	tkns = 0;
 	tokens.innerText = '000000'
+
+	console.log(`${tkns} initial`) ////////////////////////
+
 	for (let i = 0; i < 3; i++){
-		console.log(`RANDOM IMAGE PLACED IN SLOT ${i}`)
-		document.getElementById(`${i}`).src = `${slotValues[Math.floor(Math.random()*slotValues.length)].image}`;
-		document.getElementById(`${i}`).style.width = '150px';
+		setSlot(i);
+		// console.log(`RANDOM IMAGE PLACED IN SLOT ${i}`)
+		// document.getElementById(`${i}`).src = `${slotValues[Math.floor(Math.random()*slotValues.length)].image}`;
+		// document.getElementById(`${i}`).style.width = '150px';
 	}
 }
 
+function setSlot(i) {
+	document.getElementById(`${i}`).src = `${slotValues[Math.floor(Math.random()*slotValues.length)].image}`;
+	document.getElementById(`${i}`).style.width = '150px';
+}
+
 function payMe(evt) {
+
 	tkns += Math.floor(cashIn.value / 0.05);
-	tokens.innerText = tkns;
+	tokens.innerText = leadingZeros(tkns, 6);
+
+	console.log(`+ ${cashIn.value/0.05} (VIA CASH) = ${tkns} TOKENS`)
+
 	cashIn.value = '0.00';
+	cashIn.style = null;
+	placeBet.style = null;
 }
 
 function spin(evt) {
 	
 	tkns -= 5;
-	tokens.innerText = tkns;
+	tokens.innerText = leadingZeros(tkns, 6);
+	console.log(`- 5 (VIA SPIN) = ${tkns} TOKENS`)
 	spinResults = [null, null, null];
 
-	for (i = 0; i < spinResults.length; i++) {
-		// play slider animation
-		spinResults[i] = slotValues[Math.floor(Math.random()*slotValues.length)]; // puts a slotValue OBJECT into array
-		document.getElementById(`${i}`).src = spinResults[i].image;
-	}
+	for (i = 0; i < 3; i++) {
 
-	console.log(spinResults)
+		setSlot(i);
+
+		// spinResults[i] = slotValues[Math.floor(Math.random()*slotValues.length)]; // puts a slotValue OBJECT into array
+		// document.getElementById(`${i}`).src = spinResults[i].image;
+	}
+	
 	matchTally(spinResults);
 }
 
 function matchTally(objArr) {
+
 	let slotCount = spinResults.reduce(function (acc, slot) {
 		acc[slot.name] = acc[slot.name] ? acc[slot.name] + 1 : 1;
 		return acc;
 	}, {});
+
 	console.log(slotCount) // spits out objArr with names and count only
+
 	updateTokens(slotCount); // pass that objArr to calculate tokens
 }
 
 function updateTokens(obj) {
+
 	for (let key in obj) { // key = names, value = count
 
 		if(obj[key] === 3){ 
-			console.log('3 IN A ROW')
-		  if (key === 'dollar') {
-			console.log('DOLL HAIR! ADD 500')
-			tkns += 500;
-		  }
-		  else if (key !== 'dollar' && key !== 'devil') {
-			console.log('ADD 100')
-			tkns += 100;
-		  }
-		  else {
-			console.log('DEVILS! MINUS 420')
-			tkns -= 420;
-		  }
+			if (key === 'dollar') {
+				// symbol.innerText = '+';
+				// win.innerText = '500';
+				tkns += 500;
+				console.log(`+ 500 ($$$) = ${tkns}`)
+		  	}
+			else if (key === 'devil') {
+				// symbol.innerText = '-';
+				// win.innerText = '666';
+				tkns -= 420;
+				console.log(`- 666 (DEVIL ROW) = ${tkns}`)
+			}
+			else if (key === 'of') {
+				// symbol.innerText = '-';
+				// win.innerText = '40%';
+				tkns -= Math.floor(tkns * .20);
+				console.log(`- 20% (TRIPLE O.F.) = ${tkns}`)
+			}
+			else if (key === 'mv') {
+				// symbol.innerText = '-';
+				// win.innerText = '80%';
+				tkns -= Math.floor(tkns * .40);
+				console.log(`- 40% (TRIPLE M.V.) = ${tkns}`)
+			}
+			else if (key === 'kit') {
+				// symbol.innerText = '';
+				// win.innerText = '';
+				tkns = 0;
+				console.log(`YOU'RE F*CKED = ${tkns}`)
+			}
+		 	else { // key = anything else
+				// symbol.innerText = '+';
+				// win.innerText = '100';
+				tkns += 100;
+				console.log(`+ 100 (PERF ROW) = ${tkns}`)
+		  	}
 		}
 	
 		if (obj[key] === 2) {
-		  if (key !== 'devil') {
-			console.log('2 MATCHES, ADD 50')
-			tkns += 50
-		  }
-		  else {
-			console.log('2 DEVILS, MINUS 100')
-			tkns -= 100;
-		  }
+			if (key === 'devil') {
+				// symbol.innerText = '-';
+				// win.innerText = '100';
+				tkns -= 100;
+				console.log(`- 100 (DOUBLE DEVILS) = ${tkns}`)
+			}
+			else if (key === 'of') {
+				// symbol.innerText = '-';
+				// win.innerText = '40%';
+				tkns -= Math.floor(tkns * .20);
+				console.log(`- 20% (DOUBLE O.F.) = ${tkns}`)
+			}
+			else if (key === 'mv') {
+				// symbol.innerText = '-';
+				// win.innerText = '80%';
+				tkns -= Math.floor(tkns * .40);
+				console.log(`- 40% (DOUBLE M.V.) = ${tkns}`)
+			}
+			else { // key = anything else
+				// symbol.innerText = '+';
+				// win.innerText = '50';
+				tkns += 50
+				console.log(`+ 50 (DOUBLES) = ${tkns}`)
+			}
 		}
-	
+		
 		if (obj[key] === 1 && key === 'dollar') {
-			console.log('BONUS 20')
+			// symbol.innerText = '+';
+			// win.innerText = '20';
 			tkns += 20;
+			console.log(`+ 20 ($ SIGN) = ${tkns}`)
 		}
-
-		console.log(tkns)
-		tokens.innerText = tkns;
-	  }
+		
+		if (tkns > 0) {
+			tokens.innerText = leadingZeros(tkns, 6);
+		}
+		else {
+			tkns = 0;
+			tokens.innerText = '000000'
+		}
+	}
 }
+
+function leadingZeros(n, width, z) {
+	z = z || '0';
+	n = n + '';
+	return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+  }
