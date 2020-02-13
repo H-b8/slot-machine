b45b43e11556d8d44d2eca25af9ef3630269a9f7
CL <carolinemv.le@gmail.com>
2020-02-12 18:04:41 -0800

    Updates JS

--- js/main.js
+++ js/main.js
@@ -1,229 +1,204 @@
 /* * * * * * * * * * * * * CONSTANTS * * * * * * * * * * * * */
 
+
 const slotValues = [
 	{ name: 'dollar', image: 'images/dollar.png' },
 	{ name: 'pleaser', image: 'images/pleaser.png' }, 
 	{ name: 'cherries', image: 'images/cherries.png' },
-	{ name: 'kit', image: 'images/kit.png' }, 
+	{ name: 'trainingkit', image: 'images/trainingkit.png' }, 
 	{ name: 'angel', image: 'images/angel.png' },
 	{ name: 'devil', image: 'images/devil.png' },
 	{ name: 'of', image: 'images/of.png' },
 	{ name: 'mv', image: 'images/mv.png' }
-  ];
+];
+
+const highlightTargets = [a, b, c, d, e, f, g, h, j, k];
+
+
 /* * * * * * * * * * APP'S STATE VARIABLES * * * * * * * * * */
 
 
-let tkns, highlighter;
+let tkns;
 let spinResults =[];
 
 
 /* * * * * * * * * CASHED ELEMENT REFERENCES * * * * * * * * */
 
 
-let placeBet = document.querySelector('#pb'); // button
-let cashIn = document.querySelector('#cashin'); // input
-let cashOut = document.querySelector('#cashout'); // button
+let placeBet = document.querySelector('#pb');
+let cashIn = document.querySelector('#cashin');
+let cashOut = document.querySelector('#cashout');
 let spinner = document.querySelector('#spinner');
 
 
 /* * * * * * * * * * * EVENT LISTENERS * * * * * * * * * * * */
 
+
 placeBet.addEventListener('click', payMe);
 cashOut.addEventListener('click', initialize);
+
 spinner.addEventListener('click', function(evt){
+
 	if(tkns >= 5) {
         spin(evt);
     } 
     else {
-		// placeBet.style.background = 'red';
 		cashIn.style.color = 'red';
 		cashIn.style.borderColor = 'black';
 		cashIn.value = "IF YOU'RE STROKIN', TIP A TOKEN";
 	}
 });
 
+
 /* * * * * * * * * * * * * FUNCTIONS * * * * * * * * * * * * */
+
 
 initialize();
 
 function initialize() {
 	
-	// console.log('GAME STARTED')
-	
 	tkns = 0;
 	tokens.innerText = '000000';
-
-	console.log(`${tkns} initial`) ////////////////////////
-
+	console.log(`TOKENS = ${tkns}`)
 	for (let i = 0; i < 3; i++){
-		console.log(`RANDOM IMAGE PLACED IN SLOT ${i}`)
 		document.getElementById(`${i}`).src = `${slotValues[Math.floor(Math.random()*slotValues.length)].image}`;
 		document.getElementById(`${i}`).style.width = '150px';
 	}
 }
 
-// function setSlot(i) {
-// 	document.getElementById(`${i}`).src = `${slotValues[Math.floor(Math.random()*slotValues.length)].image}`;
-// 	document.getElementById(`${i}`).style.width = '150px';
-// }
-
 function payMe(evt) {
 
-	if (!isNaN(cashIn.value)){
-		
+	if (!isNaN(cashIn.value)){	
 		tkns += Math.floor(cashIn.value / 0.05);
 		tokens.innerText = leadingZeros(tkns, 6);
-		
-		console.log(`+ ${cashIn.value/0.05} (VIA CASH) = ${tkns} TOKENS`)
-	
+		console.log(`+ ${cashIn.value/0.05} (VIA CASH) = ${tkns} TOKENS`);
 		cashIn.value = '0.00';
 		cashIn.style = null;
-		// placeBet.style = null;
 	}
 	else {
 		cashIn.style.color = 'red';
 		cashIn.value = 'ENTER A VALID CASH AMOUNT';
 	}
-
 }
 
 function spin(evt) {
 
 	removeHighlight();
-	
 	tkns -= 5;
 	tokens.innerText = leadingZeros(tkns, 6);
-	console.log(`- 5 (VIA SPIN) = ${tkns} TOKENS`)
-	spinResults = [null, null, null];
-
+	console.log(`- 5 (VIA SPIN) = ${tkns} TOKENS`);
 	for (i = 0; i < 3; i++) {
-
-		spinResults[i] = slotValues[Math.floor(Math.random()*slotValues.length)]; // puts a slotValue OBJECT into array
-		document.getElementById(`${i}`).src = spinResults[i].image;
+		spinResults[i] = slotValues[Math.floor(Math.random()*slotValues.length)];
 	}
-	
-	matchTally(spinResults);
+	setSlotImage(spinResults);
 }
+
+function setSlotImage(objArr) {
+
+	setTimeout (function() {
+		document.getElementById('0').src = objArr[0].image;
+	}, 300);
+	setTimeout (function() {
+		document.getElementById('1').src = objArr[1].image;
+	}, 600);
+	setTimeout (function() {
+		document.getElementById('2').src = objArr[2].image;
+		matchTally(spinResults);
+	}, 900);
+}
+
 
 function matchTally(objArr) {
 
 	let slotCount = spinResults.reduce(function (acc, slot) {
+
 		acc[slot.name] = acc[slot.name] ? acc[slot.name] + 1 : 1;
 		return acc;
 	}, {});
-
-	console.log(slotCount) // spits out objArr with names and count only
-
-	updateTokens(slotCount); // pass that objArr to calculate tokens
+	console.log(slotCount);
+	updateTokens(slotCount);
 }
 
 function updateTokens(obj) {
 
-	for (let key in obj) { // key = names, value = count
-
+	for (let key in obj) {
 		if(obj[key] === 3){ 
 			if (key === 'dollar') {
-				// symbol.innerText = '+';
-				// win.innerText = '500';
 				d.style.background = 'yellow';
 				d.style.color = 'blue';
 				tkns += 500;
-				console.log(`+ 500 ($$$) = ${tkns}`)
+				console.log(`+ 500 ($$$) = ${tkns}`);
 		  	}
 			else if (key === 'devil') {
-				// symbol.innerText = '-';
-				// win.innerText = '666';
 				f.style.background = 'yellow';
 				f.style.color = 'blue';
 				tkns -= 420;
-				console.log(`- 666 (DEVIL ROW) = ${tkns}`)
+				console.log(`- 666 (DEVIL ROW) = ${tkns}`);
 			}
 			else if (key === 'of') {
-				// symbol.innerText = '-';
-				// win.innerText = '40%';
 				g.style.background = 'yellow';
 				g.style.color = 'blue';
 				tkns -= Math.floor(tkns * .20);
-				console.log(`- 20% (TRIPLE O.F.) = ${tkns}`)
+				console.log(`- 20% (ONLYFANS PAYCUT) = ${tkns}`);
 			}
 			else if (key === 'mv') {
-				// symbol.innerText = '-';
-				// win.innerText = '80%';
 				h.style.background = 'yellow';
 				h.style.color = 'blue';
 				tkns -= Math.floor(tkns * .40);
-				console.log(`- 40% (TRIPLE M.V.) = ${tkns}`)
+				console.log(`- 40% (MANYVIDS PAY CUT) = ${tkns}`);
 			}
-			else if (key === 'kit') {
-				// symbol.innerText = '';
-				// win.innerText = '';
+			else if (key === 'trainingkit') {
 				j.style.background = 'yellow';
 				j.style.color = 'blue';
 				tkns = 0;
-				console.log(`YOU'RE F*CKED = ${tkns}`)
+				console.log(`YOU'RE F*CKED = ${tkns}`);
 			}
-		 	else { // key = anything else
-				// symbol.innerText = '+';
-				// win.innerText = '100';
+		 	else {
 				b.style.background = 'yellow';
 				b.style.color = 'blue';
 				tkns += 100;
-				console.log(`+ 100 (PERF ROW) = ${tkns}`)
+				console.log(`+ 100 (PERF ROW) = ${tkns}`);
 		  	}
 		}
-	
 		if (obj[key] === 2) {
 			if (key === 'devil') {
-				// symbol.innerText = '-';
-				// win.innerText = '100';
 				e.style.background = 'yellow';
 				e.style.color = 'blue';
 				tkns -= 100;
-				console.log(`- 100 (DOUBLE DEVILS) = ${tkns}`)
+				console.log(`- 100 (DOUBLE DEVILS) = ${tkns}`);
 			}
 			else if (key === 'of') {
-				// symbol.innerText = '-';
-				// win.innerText = '40%';
 				g.style.background = 'yellow';
 				g.style.color = 'blue';
 				tkns -= Math.floor(tkns * .20);
-				console.log(`- 20% (DOUBLE O.F.) = ${tkns}`)
+				console.log(`- 20% (ONLYFANS PAY CUT) = ${tkns}`);
 			}
 			else if (key === 'mv') {
-				// symbol.innerText = '-';
-				// win.innerText = '80%';
 				h.style.background = 'yellow';
 				h.style.color = 'blue';
 				tkns -= Math.floor(tkns * .40);
-				console.log(`- 40% (DOUBLE M.V.) = ${tkns}`)
+				console.log(`- 40% (MANYVIDS PAY CUT) = ${tkns}`);
 			}
-			else if (key === 'kit') {
-				i.style.background = 'yellow';
+			else if (key === 'trainingkit') {
+				k.style.background = 'yellow';
+				k.style.color = 'blue';
 				tkns -= Math.floor(tkns * .50);
-				i.style.background = 'yellow';
-				i.style.color = 'blue';
-				console.log(`- 50% (DOUBLE T.K.) = ${tkns}`)
+				console.log(`- 50% (DOUBLE T.K.) = ${tkns}`);
 			}
-			else { // key = anything else
-				// symbol.innerText = '+';
-				// win.innerText = '50';
+			else {
 				a.style.background = 'yellow';
 				a.style.color = 'blue';
 				tkns += 50
-				console.log(`+ 50 (DOUBLES) = ${tkns}`)
+				console.log(`+ 50 (DOUBLES) = ${tkns}`);
 			}
 		}
-		
 		if (obj[key] === 1 && key === 'dollar') {
-			// symbol.innerText = '+';
-			// win.innerText = '20';
 			c.style.background = 'yellow';
 			c.style.color = 'blue';
-			c.style.fontWeight = '600';
 			tkns += 20;
-			console.log(`+ 20 ($ SIGN) = ${tkns}`)
+			console.log(`+ 20 ($ SIGN) = ${tkns}`);
 		}
-		
+
 		if (tkns > 0) {
 			tokens.innerText = leadingZeros(tkns, 6);
 		}
@@ -235,6 +210,7 @@ function updateTokens(obj) {
 }
 
 function removeHighlight() {
+
 	a.style = null;
 	b.style = null;
 	c.style = null;
@@ -243,11 +219,12 @@ function removeHighlight() {
 	f.style = null;
 	g.style = null;
 	h.style = null;
-	i.style = null;
+	k.style = null;
 	j.style = null;
 }
 
 function leadingZeros(n, width, z) {
+
 	z = z || '0';
 	n = n + '';
 	return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
